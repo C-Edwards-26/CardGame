3f10c0c4dc1f8f8ba85bbbef902b950107a80805
C-Edwards-26 <143653695+C-Edwards-26@users.noreply.github.com>
2025-11-18 09:51:23 -0800

    CReated files and completed Card class

---
+++ src/main/java/Card.java
@@ -0,0 +1,39 @@
+public class Card {
+    private int rank;
+    private String suit;
+    private int value;
+
+    public Card(int rank, String suit, int value) {
+        this.rank = rank;
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
