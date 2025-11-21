abd67f434806696dd2662c0e7aec702a4d7a91c4
C-Edwards-26 <143653695+C-Edwards-26@users.noreply.github.com>
2025-11-21 09:40:21 -0800

    Started on the logic for my game and moved everything to playGame instead of main method.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,24 +1,41 @@
+import java.util.Scanner;
+
 public class Game {
     private Player players;
     private Deck deck;
+    public final static String[] totalRanks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    public final static String[] totalSuits = {"Hearts", "Clubs", "Spades", "Diamonds"};
+    public final static int[] totalValues = {1,2,3,4,5,6,7,8,9,10,10,10,10};
 
     public Game(Player players, Deck deck) {
         this.players = players;
         this.deck = deck;
     }
 
+    public Game() {
+    }
+
+    public boolean askOpponents(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many opponents would you like to face, one or two?");
+        int opponents = input.nextInt();
+        while (opponents != 1 && opponents != 2) {
+            System.out.println("Incorrect ammount of opponents!");
+            System.out.println("How many opponents would you like to face, one or two?");
+            opponents = input.nextInt();
+        }
+        if (opponents == 1) {
+            return true;
+        }
+        return false;
+    }
+
     public void printInstructions() {
         return;
     }
     public void playGame() {
-        return;
-    }
-
-    public static void main(String[] args) {
-        String[] ranks = {"A", "2", "3"};
-        String[] suits = {"Hearts", "Clubs"};
-        int[] values = {1,2,3};
-        Deck newDeck = new Deck(ranks, suits, values);
+        Deck newDeck = new Deck(totalRanks, totalSuits, totalValues);
+        askOpponents();
         System.out.println(newDeck);
         newDeck.shuffle();
         System.out.println(newDeck);
@@ -29,5 +46,10 @@ public class Game {
         }
         Chase.addPoints(10);
         System.out.println(Chase);
+    }
+
+    public static void main(String[] args) {
+        Game g1 = new Game();
+        g1.playGame();
     }
 }
