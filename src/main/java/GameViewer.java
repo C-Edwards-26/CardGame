a91263b789f7133bbefb0e223cad8dc36141a4c6
C-Edwards-26 <143653695+C-Edwards-26@users.noreply.github.com>
2026-02-11 11:51:08 -0800

    Started and created new frontend Class

---
+++ src/main/java/GameViewer.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameViewer extends JFrame {
+    private Game window;
+    private Image[] cardImages;
+    private Image backgroundImage;
+    private Image mainBackground;
+}
